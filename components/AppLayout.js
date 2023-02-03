2b926c6b38dfd7a666570d28f2fed40955658be2
saiani1 <saiani@naver.com>
2023-02-03 09:59:26 +0900

    first commit

---
+++ components/AppLayout.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { Menu, Input, Row, Col } from "antd";
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://velog.io/@saiani1"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by BonnieC
+          </a>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
